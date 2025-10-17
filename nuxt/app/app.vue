000337c087a2847a90490de97262ef52e06ddd48
Christian Andrei Reyes <careyes@sscgi.com>
2025-10-17 09:57:45 +0800

    add tailwindcss

--- nuxt/app/app.vue
+++ nuxt/app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nuxt-layout>
+      <nuxt-page></nuxt-page>
+    </nuxt-layout>
   </div>
 </template>
---
+++ nuxt/app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="bg-red-300">Hello from index</div>
+</template>
